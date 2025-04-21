58f6e764d76ba50d3d06046247203623d91e6bd2
Smiley <y910906@gmail.com>
2025-04-21 23:19:02 +0800

    basic layout

--- README.md
+++ README.md
@@ -37,4 +37,3 @@ npm run build
 ```sh
 npm run lint
 ```
-# AWSHackthon2025
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <BasicLayout>
+    <template v-slot:user-info>
+      <div>User2</div>
+    </template>
+    <template v-slot:notification>
+      <div>Notification2</div>
+    </template>
+    <template v-slot:main-content>
+      <div>
+        Content2
+      </div>
+    </template>
+  </BasicLayout>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '../components/layouts/BasicLayout.vue'
+</script>
+
