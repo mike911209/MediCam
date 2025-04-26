79c0c761a8a608a2dc5d741d58854662bb56f40e
牜攵 <nevikw39@cs.nthu.edu.tw>
2025-04-26 23:07:10 +0800

    Recorder with Transcribe Stream!!

--- icam/RecorderTranscribe/recorder_transcribe.py
+++ icam/RecorderTranscribe/recorder_transcribe.py
@@ -1,75 +1,68 @@
 #!/usr/bin/python
 
 import argparse
+import asyncio
 import dotenv
 import logging
 import json
-import os
-import time
-import requests
 
 from datetime import datetime
 
+from amazon_transcribe.client import TranscribeStreamingClient
+from amazon_transcribe.handlers import TranscriptResultStreamHandler
+
 import boto3
-import numpy as np
-import scipy.io.wavfile as wav
 import sounddevice as sd
 
 
 SAMPLE_RATE = 16000  # Audio sample rate (Hz)
+BLOCK_SIZE = 1024 # Stream block size
 THRESHOLD = 1e-4  # Volume threshold to decide "not muted"
-BUCKET_TEXT = "unapproved-orders"  # S3 bucket name
-BUCKET_RECORDING = "medicam-recordings"  #
+BUCKET = "unapproved-orders"  # S3 bucket name
 
 
-def main():
-    s3 = boto3.client("s3")
-    transcribe = boto3.client('transcribe')
+class Handler(TranscriptResultStreamHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.s3 = boto3.client("s3")
 
-    while True:
-        audio = sd.rec(10 * SAMPLE_RATE, samplerate=SAMPLE_RATE, channels=1)
-        sd.wait()
+    async def handle_transcript_event(self, transcript_event):
+        transcript = ''.join(result.alternatives[0].transcript for result in transcript_event.transcript.results if not result.is_partial and len(result.alternatives))
+        if not transcript:
+            logging.debug("No transcript.")
+            return
+        logging.debug(f"Transcript: {transcript}")
+        print(transcript)
 
-        avg_vol = np.abs(audio).mean()
-        logging.debug(f"avg. vol.: {avg_vol}")
+        name = datetime.now().isoformat(timespec="seconds")
+        with open(f"/tmp/{name}.json", 'w') as f:
+            json.dump({"date": name, "text": transcript}, f, ensure_ascii=False)
+        self.s3.upload_file(f"/tmp/{name}.json", BUCKET, f"{name}.txt")
 
-        if True: # avg_vol > THRESHOLD:
-            job = datetime.now().isoformat(timespec="seconds").replace(':', '-')
-            path = f"/tmp/{job}.wav"
 
-            logging.info(f"Saving {path}...")
-            wav.write(path, SAMPLE_RATE, (audio * 32767).astype(np.int16))
+async def transcribe_streaming():
+    ts = TranscribeStreamingClient(region="us-east-1")
 
-            logging.info(f"Uploading {path}...")
-            s3.upload_file(path, BUCKET_RECORDING, f"{job}.wav")
+    stream = await ts.start_stream_transcription(
+        language_code="zh-TW",
+        media_sample_rate_hz=SAMPLE_RATE,
+        media_encoding="pcm"
+    )
+    
+    handler = Handler(stream.output_stream)
 
-            logging.info(f"Transcribing {path}...")
-            transcribe.start_transcription_job(
-                TranscriptionJobName=job,
-                Media={'MediaFileUri': f"s3://{BUCKET_RECORDING}/{job}.wav"},
-                MediaFormat='wav',
-                IdentifyMultipleLanguages=True,
-                LanguageOptions=('en-US', 'zh-TW'),
-            )
+    async def send():
+        with sd.InputStream(samplerate=SAMPLE_RATE, channels=1, dtype="int16", blocksize=BLOCK_SIZE) as device:
+            while True:
+                audio_chunk, _ = device.read(BLOCK_SIZE)
+                await stream.input_stream.send_audio_event(audio_chunk=audio_chunk.tobytes())
 
-            while (result := transcribe.get_transcription_job(TranscriptionJobName=job))['TranscriptionJob']['TranscriptionJobStatus'] not in ('COMPLETED', 'FAILED'):
-                time.sleep(2)
-            
-            if result['TranscriptionJob']['TranscriptionJobStatus'] == 'FAILED':
-                logging.error(f"Transcription failed: {result['TranscriptionJob']['FailureReason']}!!")
-                continue
+    async def receive(): await handler.handle_events()
 
-            url = result['TranscriptionJob']['Transcript']['TranscriptFileUri']
-            response = requests.get(url).json()['results']['transcripts'][0]['transcript']
-            logging.debug(f"response: {response}")
-            
-            with open(f"/tmp/{job}.json", 'w') as f:
-                json.dump({"date": job, "text": response}, f)
-            s3.upload_file(f"/tmp/{job}.json", BUCKET_TEXT, f"{job}.json")
+    await asyncio.gather(send(), receive())
 
-            logging.info(f"Cleaning up {job}...")
-            os.remove(f"/tmp/{job}.wav")
-            os.remove(f"/tmp/{job}.json")
+
+def main(): asyncio.run(transcribe_streaming())
 
 
 if __name__ == "__main__":
